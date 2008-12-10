919f287d6c66c25b9f48364621aa8fbda5e60ce4
Daniel Westermann-Clark <dwc@ufl.edu>
2008-12-10 22:18:34 +0000

    Update Trac post-commit hook for Trac 0.11 (removed some options and moved some stuff around)

--- script/ufl_webadmin_svn_postcommit_trac.sh
+++ script/ufl_webadmin_svn_postcommit_trac.sh
@@ -7,14 +7,11 @@ REV="$2"
 LOG="$($SVNLOOK log -r "$REV" "$REPO")"
 AUTHOR="$($SVNLOOK author -r "$REV" "$REPO")"
 
-TRAC_DIR="/var/lib/trac/$(basename $REPO)"
+TRAC_DIR="/var/lib/trac/env/$(basename $REPO)"
 TRAC_URL="http://trac.webadmin.ufl.edu/$(basename $REPO)"
 
 if [ -d "$TRAC_DIR" ]; then
-    /usr/bin/python /usr/bin/ufl_webadmin_svn_postcommit_trac.py \
+    /usr/bin/python /usr/share/doc/trac-*/contrib/trac-post-commit-hook \
 	-p "$TRAC_DIR" \
-	-r "$REV" \
-	-u "$AUTHOR" \
-	-m "$LOG" \
-	-s "$TRAC_URL"
+	-r "$REV"
 fi
