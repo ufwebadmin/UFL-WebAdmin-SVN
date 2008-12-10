5851a84aa7cde199a3a89e5955e68d55ed84fc64
Daniel Westermann-Clark <dwc@ufl.edu>
2008-12-10 22:22:18 +0000

    Remove svnlook calls to save a little time

--- script/ufl_webadmin_svn_postcommit_trac.sh
+++ script/ufl_webadmin_svn_postcommit_trac.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-SVNLOOK=/usr/bin/svnlook
-
 REPO="$1"
 REV="$2"
-LOG="$($SVNLOOK log -r "$REV" "$REPO")"
-AUTHOR="$($SVNLOOK author -r "$REV" "$REPO")"
 
 TRAC_DIR="/var/lib/trac/env/$(basename $REPO)"
-TRAC_URL="http://trac.webadmin.ufl.edu/$(basename $REPO)"
 
 if [ -d "$TRAC_DIR" ]; then
     /usr/bin/python /usr/share/doc/trac-*/contrib/trac-post-commit-hook \
